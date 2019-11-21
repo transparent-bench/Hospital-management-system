364c0127a64cbda842069ebe71b6d46117799574
Egor <egor.birdi7@gmail.com>
2019-11-21 19:19:55 +0300

    Add solution for select 1

---
+++ src/scripts/select_1.sql
@@ -0,0 +1,26 @@
+select * from staff
+inner join (
+    select distinct doctor_id
+    from appointment_patient_doctor_relation apdr1
+    inner join (
+        select id
+        from appointment app
+        where occurence_date IN (
+            select occurence_date
+            from appointment
+                     left join patient p
+                               on p.phone_num = '1234556'
+                     left join appointment_patient_doctor_relation apdr2
+                               on appointment.id = apdr2.appointment_id and
+                                  apdr2.patient_id = p.id
+            order by occurence_date DESC
+            limit 1
+        )
+    ) appointments on apdr1.appointment_id = appointments.id
+) doctor_ids on doctor_ids.doctor_id = staff.id
+where staff.position = 'doctor' and (
+    staff.first_name similar to '(M|L)%' and not staff.last_name similar to '(M|L)%'
+    or
+    not staff.first_name similar to '(M|L)%' and staff.last_name similar to '(M|L)%'
+)
+
---
+++ src/scripts/test_data_select_1.sql
@@ -0,0 +1,40 @@
+insert into auth(login, password1, name) VALUES('egorka', 1234, 'Egor');
+insert into auth(login, password1, name) VALUES ('alinochka', 456, 'Alina');
+insert into auth(login, password1, name) VALUES ('kamal', 798, 'Kamil');
+insert into auth(login, password1, name) VALUES ('marinochka', 3867, 'Marina');
+
+insert into passport(seria, number, birth, f_name, l_name, gender, address)
+    VALUES (1234, 567890, '1999-10-15', 'Kamil', 'Rizatdinov', 'male', 'Universitetskaya-1');
+
+insert into patient(bank_account_id, auth_id, phone_num, insurance_policy_id, passport_seria, passport_number)
+    VALUES (Null, 3, 1234556, 173875, 1234, 567890);
+
+insert into staff(first_name, last_name, auth_id, birthday, position, gender)
+    VALUES ('Mazarello', 'Cool last name', 1,  '2001-01-16', 'doctor', 'male');
+insert into staff(first_name, last_name, birthday, position, gender)
+    VALUES ('first name', 'Mazaro', '2001-01-16', 'doctor', 'male');
+insert into staff(first_name, last_name, birthday, position, gender)
+    VALUES ('Mazarello', 'M Last name', '2001-01-16', 'doctor', 'male');
+
+insert into staff(first_name, last_name, birthday, position, gender)
+    VALUES ('Wrong doctor', 'M Last name', '2001-01-16', 'doctor', 'male');
+
+
+insert into appointment(occurence_date, diagnosis, description, reason_to_create)
+    VALUES ('2019-10-03', 'obesity', 'he is too fat', 'make him smaller');
+insert into appointment(occurence_date, diagnosis, description, reason_to_create)
+    VALUES ('2019-10-03', 'Stress', 'description', 'reason');
+insert into appointment(occurence_date, diagnosis, description, reason_to_create)
+    VALUES ('2019-10-03', 'obesity', 'he is too fat', 'make him smaller');
+insert into appointment(occurence_date, diagnosis, description, reason_to_create)
+    VALUES ('2019-9-03', 'old obesity', 'he is too fat', 'make him smaller');
+
+
+insert into appointment_patient_doctor_relation(appointment_id, patient_id, doctor_id)
+    VALUES (1, 1, 1);
+insert into appointment_patient_doctor_relation(appointment_id, patient_id, doctor_id)
+    VALUES (2, 1, 2);
+insert into appointment_patient_doctor_relation(appointment_id, patient_id, doctor_id)
+    VALUES (3, 1, 3);
+insert into appointment_patient_doctor_relation(appointment_id, patient_id, doctor_id)
+    VALUES (4, 1, 4);
