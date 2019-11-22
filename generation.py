ba6ab66304f7bffaf4e9ad2a138a9297b98fda17
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-11-22 14:42:19 +0300

    Add staff generator

--- generation.py
+++ generation.py
@@ -1,9 +1,19 @@
 from src.utils.database import Database
-from faker import Faker
-import random
+from mimesis import Generic
+from mimesis import Person
+from mimesis import Datetime
 
-fake = Faker('en_US')
-print(fake.past_date())
+p = Person('en')
+g = Generic('en')
+d = Datetime()
+
+print(g.random.custom_code(mask='####'))
+print(g.random.custom_code(mask='######'))
+print(p.name())
+print(p.last_name())
+print(p.password(length=10))
+print(p.email())
+print(d.date(start=1900))
 
 with Database() as db:
     db.open(dbname='hospital_management_system',
@@ -11,31 +21,50 @@ with Database() as db:
             password='',
             host='localhost')
 
+
+
     for _ in range(100):
         db.write('auth',
                  'login, password1, name',
-                 "'{}', '{}', '{}'".format(fake.word(),
-                                           fake.password(),
-                                           fake.word()))
+                 "'{}', '{}', '{}'".format(p.email(),
+                                           g.random.custom_code(mask='@@####@@##'),
+                                           p.name()))
 
     for _ in range(100):
         db.write('passport',
                  'seria, number, birth, f_name, l_name, gender, address',
-                 "{}, {}, '{}', '{}', '{}', '{}', '{}'".format(random.randint(1000, 9999),
-                                                             random.randint(100000, 999999),
-                                                             fake.past_date(),
-                                                             fake.name().split()[0],
-                                                             fake.name().split()[1],
-                                                             random.choice(['male', 'female']),
-                                                             fake.address()))
+                 "{}, {}, '{}', '{}', '{}', '{}', '{}'".format(g.random.custom_code(mask='####'),
+                                                             g.random.custom_code(mask='######'),
+                                                             d.date(start=1900),
+                                                             p.name(),
+                                                             p.surname(),
+                                                             p.random.choice(['male', 'female']),
+                                                             p.occupation()))
 
+    auth_ids = [auth_id for auth_id, in db.get('auth', 'id')]
     for _ in range(100):
+        auth_id = g.random.choice(auth_ids)
+        auth_ids.remove(auth_id)
+        position = g.random.choice(['doctor', 'administrator', 'nurse', 'security', 'IT-administrator'])
+        gender = g.random.choice(['male', 'female'])
+
         db.write('staff',
-                 'name, number, birth, f_name, l_name, gender, address',
-                 "{}, {}, '{}', '{}', '{}', '{}', '{}'".format(random.randint(1000, 9999),
-                                                             random.randint(100000, 999999),
-                                                             fake.past_date(),
-                                                             fake.name().split()[0],
-                                                             fake.name().split()[1],
-                                                             random.choice(['male', 'female']),
-                                                             fake.address()))
+                 'first_name, last_name, room, auth_id, birthday, position, gender',
+                 "'{}', '{}', {}, {}, '{}', '{}', '{}'".format(p.name(),
+                                                     p.surname(),
+                                                     g.random.randint(1, 100),
+                                                     auth_id,
+                                                     d.date(start=1900),
+                                                     position,
+                                                     gender))
+
+    query = "select id from staff s where s.position = 'security'"
+    staff_ids = [staff_id for staff_id, in db.query(query)]
+    for _ in range(100):
+        staff_id = g.random.choice(staff_ids)
+        staff_ids.remove(staff_id)
+        db.write('camera',
+                 'name, location, staff_id',
+                 "'{}', '{}', {}".format(p.name(),
+                                         (g.random.randint(0, 180), g.random.randint(0, 180)),
+                                         staff_id))
