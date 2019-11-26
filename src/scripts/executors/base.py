5e01a04da62a90012eb82459616164ada55b2ed6
Egor <egor.birdi7@gmail.com>
2019-11-26 23:05:25 +0300

    add feature to BaseExecutor.fetch with options

--- src/scripts/executors/base.py
+++ src/scripts/executors/base.py
@@ -10,11 +10,13 @@ class BaseExecutor(ABC):
     def file_name(self) -> str:
         raise NotImplementedError
 
-    def fetch(self):
+    def fetch(self, *options):
         with Database() as db:
             db.open(dbname="hospital_management_system", user="postgres", password="", host="localhost")
             with open(self.file_name, 'r') as file:
                 sql_query = file.read()
+                if options:
+                    sql_query = sql_query.format(*options)
             db.cursor.execute(sql_query)
             results = db.cursor.fetchall()
             return results
