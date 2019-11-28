d2f2e46fe5d2166fc61b0b6cc005f6d257e6eed4
Egor <egor.birdi7@gmail.com>
2019-11-28 18:43:17 +0300

    update test data for select 3

--- src/scripts/test_data/select_3.py
+++ src/scripts/test_data/select_3.py
@@ -8,22 +8,26 @@ from src.utils.generation import (
 
 
 def populate(is_print=False):
-    patient = create_patient(is_print=is_print)
-    base_datetime = datetime.today()
-    datetimes_of_appointments = [base_datetime - timedelta(weeks=i) for i in range(0, 4)]
-    appointments = []
-    appointments.extend(
-        [create_appointment(occurrence_date=occ_date, is_print=is_print) for occ_date in datetimes_of_appointments]
-    )
-    appointments.extend(
-        [
-            create_appointment(occurrence_date=occ_date + timedelta(hours=-1), is_print=is_print,)
-            for occ_date in datetimes_of_appointments
-        ]
-    )
+    patients = []
+    for _ in range(3):
+        patients.append(create_patient(is_print=is_print))
 
-    for _appointment in appointments:
-        create_appointment_patient_doctor_relation(patient=patient, appointment=_appointment, is_print=is_print)
+    for patient in patients:
+        base_datetime = datetime.today()
+        datetimes_of_appointments = [base_datetime - timedelta(weeks=i) for i in range(0, 4)]
+        appointments = []
+        appointments.extend(
+            [create_appointment(occurrence_date=occ_date, is_print=is_print) for occ_date in datetimes_of_appointments]
+        )
+        appointments.extend(
+            [
+                create_appointment(occurrence_date=occ_date + timedelta(hours=-1), is_print=is_print,)
+                for occ_date in datetimes_of_appointments
+            ]
+        )
+
+        for _appointment in appointments:
+            create_appointment_patient_doctor_relation(patient=patient, appointment=_appointment, is_print=is_print)
 
 
 def main():
