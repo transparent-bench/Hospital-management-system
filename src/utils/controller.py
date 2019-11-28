b53dafb0bde1f9c7d5f8820ae9d75a8220465189
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-11-28 22:28:29 +0300

    refactored fetch_by_index function, now it passes *options and **kwargs to subclass' fetch() method

--- src/utils/controller.py
+++ src/utils/controller.py
@@ -1,9 +1,9 @@
 from src.scripts.executors import BaseExecutor
 
 
-def fetch_by_index(index):
-    return BaseExecutor.get_subclass(index)().fetch(index)
+def fetch_by_index(index, *options, **kwargs):
+    return BaseExecutor.get_subclass(index)().fetch(*options, **kwargs)
 
 
 if __name__ == '__main__':
-    print(fetch_by_index('drop_and_create'))
+    print(fetch_by_index('1', '+39950937634'))
