5a0b032b4966c4f6a4aed5eedc760c7a8e88a3ed
Alina <a.paukova@innopolis.university>
2019-11-28 17:06:50 +0300

    merge remote updates

--- src/scripts/executors/create_schema.py
+++ src/scripts/executors/create_schema.py
@@ -5,4 +5,4 @@ from pathlib import Path
 
 class CreateSchemaExecutor(BaseExecutor):
     file_name = Path(__file__).parent.parent / "create_schema.sql"
-    index = '.'
+    index = 'create_schema'
--- src/utils/controller.py
+++ src/utils/controller.py
@@ -1,1 +1,9 @@
-# todo
+from src.scripts.executors import BaseExecutor
+
+
+def fetch_by_index(index):
+    return BaseExecutor.get_subclass(index)().fetch(index)
+
+
+if __name__ == '__main__':
+    print(fetch_by_index('1'))
